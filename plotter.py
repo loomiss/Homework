936275cd7ceb939e8b48c37a8f3b99b9802c8131
Samuel <smlloomis@gmail.com>
2013-11-18 18:04:49 -0800

    General workspace cleanup and plotter python code created

--- plotter.py
+++ plotter.py
@@ -1,20 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-Data = np.genfromtxt('411labp2asmall.csv', delimiter=',', names=True)
+path = "sam_lab2/"
+filename = "4c.csv"
+
+
+Data = np.genfromtxt('%s%s'% (path, filename), delimiter=',')
 
 
 # print(Data)
-# print(Data.dtype.names)
+# print(Data[:,1])
+# print(len(Data[0,:]))
+# print(len(Data[1,:]))
+# print(len(Data[2,:]))
 
+#channel 1
 plt.figure()
+plt.plot(Data[:,0],Data[:,1], 'b')
+plt.title('Channel 1')
 
-plt.plot(Data[Data.dtype.names[0]], Data[Data.dtype.names[1]], label='')
-plt.plot(Data[Data.dtype.names[0]], Data[Data.dtype.names[1]], label='')
-plt.plot(Data[Data.dtype.names[0]], Data[Data.dtype.names[1]], label=Data.dtype.names[1])
-plt.title('Current as a function of Voltage through a small graphite rod')
-plt.xlabel('Volts (V)')
-plt.ylabel('Current (A)')
-plt.legend(loc='best')
-plt.savefig('411labp2asmall.pdf')
+#channel 2
+plt.figure()
+plt.plot(Data[:,0],Data[:,2], 'g')
+plt.title('Channel 2')
+
+#channels 1,2
+plt.figure()
+plt.plot(Data[:,0],Data[:,1], 'b')
+plt.plot(Data[:,0],Data[:,2], 'g')
+plt.title('Channel 1 & 2')
+
+# plt.title('')
+# plt.xlabel('Volts (V)')
+# plt.ylabel('Current (A)')
+# plt.legend(loc='best')
+plt.show()
 
